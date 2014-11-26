211b89bc0c2d1391c110ccf060fa92825fe04ad1
geyx <bruce.g@163.com>
2014-11-26 16:09:45 +0800

    * 修改生成zip批处理内容 + 添加默认version文件

--- gen_viewhome.bat
+++ gen_viewhome.bat
@@ -1,10 +1,20 @@
-if exist  build  (
-	echo "存在build文件夹"
-)else (
-  echo "不存在build文件夹，创建build文件夹"
-  md build
+if  exist  build  (
+	echo "存在build文件夹，删除build文件夹"
+	rd build
 )
+md build\viewhome\assets
+md build\viewhome\config
+md build\viewhome\html
+md build\viewhome\xml
+
+xcopy /S assets build\viewhome\assets
+xcopy /S config build\viewhome\config
+xcopy /S html build\viewhome\html
+xcopy /S xml build\viewhome\xml
+copy version build\viewhome
+cd build
 @echo "进行viewhome.zip压缩"
-zip -r build/viewhome.zip assets/ config/ html/ lib/ xml/
+zip -r viewhome.zip viewhome
 @echo "viewhome.zip已生成，文件位置build/viewhome.zip"
 pause
+rd /s /q viewhome
