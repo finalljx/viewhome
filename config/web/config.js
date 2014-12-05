c7693a1dbcd3452e4f93b4be4ffccf58a9c62c0c
geyx <bruce.g@163.com>
2014-12-05 09:56:48 +0800

    * 替换首页图片 * 修改登录验证码使用http协议

--- config/web/config.js
+++ config/web/config.js
@@ -4,9 +4,10 @@
 			"oaServerName":"mobile2.sugon.com",
 			"oaMsgServer":"mobile2.sugon.com",
 			"oaMobileServer":"mobile2.sugon.com",
-			"serverBaseUrl":"http://192.168.1.130:90/",
+			"serverBaseUrl":"https://mobile.genertec.com.cn/",
 			"encryptKey":"horiTech",
-			"appServerHost":"http://192.168.1.130:90/",
+			"appServerHost":"https://mobile.genertec.com.cn/",
+			"appServerHost2":"http://mobile.genertec.com.cn:8981/",
 			"appKey":"mobile",
 			"docServer":"doctest"
 		};
