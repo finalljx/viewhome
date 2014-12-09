e3a852a34eb392665e3ceb9c2db74e67551e8422
hongyu49 <javawhy@163.com>
2014-12-09 11:22:13 +0800

    配置serverName

--- config/web/config.js
+++ config/web/config.js
@@ -2,12 +2,12 @@
 	hori.getconfig=function(){
 
 		return{
-			"oaServerName":"OAServer01/crsc",
-			"oaMsgServer":"OAServer01/crsc",
-			"oaMobileServer":"OAServer01/crsc",
-			"serverBaseUrl":"http://192.168.12.89:90/",
+			"oaServerName":"MSG01/Server/CRSC",
+			"oaMsgServer":"MSG01/Server/CRSC",
+			"oaMobileServer":"MSG01/Server/CRSC",
+			"serverBaseUrl":"http://192.168.12.90:90/",
 			"encryptKey":"horiTech",
-			"appServerHost":"http://192.168.12.89:90/",
+			"appServerHost":"http://192.168.12.90:90/",
 			"appKey":"mobiletonghao"
 		};
 	};
