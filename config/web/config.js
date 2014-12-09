300fdcb30e2bbbaa3c9c5fdadf93e0661505e8e9
Guanghui-Wang <13069722546@163.com>
2014-12-09 15:50:07 +0800

    congfig

--- config/web/config.js
+++ config/web/config.js
@@ -2,12 +2,12 @@
 	hori.getconfig=function(){
 
 		return{
-			"oaServerName":"OAServer01/crsc",
-			"oaMsgServer":"OAServer01/crsc",
-			"oaMobileServer":"OAServer01/crsc",
-			"serverBaseUrl":"http://192.168.12.89:90/",
+			"oaServerName":"MSG01/Server/CRSC",
+			"oaMsgServer":"MSG01/Server/CRSC",
+			"oaMobileServer":"MSG01/Server/CRSC",
+			"serverBaseUrl":"http://localhost:8080/",
 			"encryptKey":"horiTech",
-			"appServerHost":"http://192.168.12.89:90/",
+			"appServerHost":"http://localhost:8080/",
 			"appKey":"mobiletonghao"
 		};
 	};
