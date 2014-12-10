0b0e7baf1978d0730047b82edcfe167d3b358be3
hongyu49 <javawhy@163.com>
2014-12-10 16:34:47 +0800

    配置

--- config/web/config.js
+++ config/web/config.js
@@ -5,9 +5,9 @@
 			"oaServerName":"MSG01/Server/CRSC",
 			"oaMsgServer":"MSG01/Server/CRSC",
 			"oaMobileServer":"MSG01/Server/CRSC",
-			"serverBaseUrl":"http://localhost:8080/",
+			"serverBaseUrl":"http://localhost:90/",
 			"encryptKey":"horiTech",
-			"appServerHost":"http://localhost:8080/",
+			"appServerHost":"http://localhost:90/",
 			"appKey":"mobiletonghao"
 		};
 	};
